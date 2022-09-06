124dbe648ab94bb0703673280d07770667f3b1b6
Ilkin Guluzada <glzdreal@gmail.com>
2022-09-06 13:00:49 +0400

    optimized

--- Tapshiriq_3/app.js
+++ Tapshiriq_3/app.js
@@ -1,23 +1,25 @@
-let newArr=[]
-
 let arr = [
-{ type:'A', value:'Tesla' },
-{ type:'A', value:'GMC' },
-{ type:'B', value:'KIA'},
-{ type:'C', value:'Dodge' },
-{ type:'C', value:'Ford' }
+    { type: "A", value: "Tesla" },
+    { type: "A", value: "GMC" },
+    { type: "B", value: "KIA" },
+    { type: "C", value: "Dodge" },
+    { type: "C", value: "Ford" },
+    { type: "C", value: "Ali" },
 ]
 
 
-arr.forEach((elem) => {
-    const obj = {}
-    obj.type = elem.type
-    let sameTypes = arr.filter((_elem) => elem.type === _elem.type)
-    arr = arr.filter((_) => _.type !== elem.type)
-    sameTypes = sameTypes.map((_) => _.value)
-    if (sameTypes.length !== 0) {
-        obj.value = sameTypes
-        newArr.push(obj)
+function accum(acc, { type, value }) {
+    const found = acc.find((_) => _.type === type)
+    if (!found) {
+        return [...acc, { type, value }];
+    } else {
+        const index = acc.indexOf(found)
+        acc.splice(index, 1)
+        let newObj = { ...found }
+        newObj.value = [newObj.value, value].flat()
+        return [...acc, newObj]
     }
-})
+}
+
+const newArr = arr.reduce(accum, []);
 console.log(newArr);
